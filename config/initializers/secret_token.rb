21dfee0ce11bed60f54b158f36da07a947ef5f56
mostlyclicks <carlos@mostlyclicks.com>
2012-04-20 20:22:34 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ridetime::Application.config.secret_token = '73cce6b2d866a4a22088632979649e4ea129aa9ed7fc9b272ef8658770441c0a20615cb9f79a5d935156bb17b78691ae5341efc39e33138a2cd2b2637cca0e38'
